53a3b1b35ac024a3587544c845a38e9194185716
Kate Owens <kate.owens@deque.com>
2023-04-20 12:44:44 -0400

    now getting a chance to work with the package stuff since we have a new xcframework that exists zipped with a checksum calculated

--- Package.swift
+++ Package.swift
@@ -14,15 +14,13 @@ let package = Package(
             name: "testFramework",
             targets: ["testFramework"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-         .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "testFramework",
-            path: "testFramework.xcframework")
+            url: "https://github.com/kateowens12/testFramework/releases/tag/1.0",
+            checksum: "518ec74cb671ecb22ed84bdb1f027bd2686a375c4d5fd9a9849f7838103012d8"
+        )
     ]
 )
