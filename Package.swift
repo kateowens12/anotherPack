8ac5eb668389e3b996c47851433ddf8f1b39bf6f
Kate Owens <kate.owens@deque.com>
2023-04-25 15:19:22 -0400

    comitting now

--- Package.swift
+++ Package.swift
@@ -9,18 +9,15 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "testFramework",
             targets: ["testFramework"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "testFramework",
-            url: "https://github.com/kateowens12/testFramework/releases/tag/1.0",
-            checksum: "518ec74cb671ecb22ed84bdb1f027bd2686a375c4d5fd9a9849f7838103012d8"
+            url: "https://github.com/kateowens12/testFramework/archive/refs/tags/1.4.zip",
+            checksum: "ae3b76f48758d4e7e1a3a3438243706a5395abe3b0ff231d6646845cef389dc2"
         )
     ]
 )
--- Sources/anotherPack/anotherPack.swift
+++ Sources/anotherPack/anotherPack.swift
@@ -1,3 +1,4 @@
+import testFramework
 public struct anotherPack {
     public private(set) var text = "Hello, World!"
 
