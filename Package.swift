0e86fddd6792f74ea15c4ff68002945d7211e70c
Kate Owens <kate.owens@deque.com>
2023-04-26 10:50:10 -0400

    packge revert

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "testFramework",
-            url: "https://github.com/kateowens12/testFramework/archive/refs/tags/1.4.3.zip",
+            url: "https://github.com/kateowens12/testFramework/archive/refs/tags/1.4.1.zip",
             checksum: "c1fb65e6211d7ba579daeef22dbd184d0032ea5f50563e00c87521a98a4956ad"
         )
     ]
